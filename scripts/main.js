5d5484dc01698e36df5b2493e542a61a63350b02
proto <schoolgreenfield63@gmail.com>
2022-11-13 09:29:52 +0545

    updated v# removing mechanism

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,34 @@
-
-let anchorElem = document.querySelectorAll(".nav-links a");
-anchorElem.forEach(elem => {
-	if (elem.href == location.href) {
-		elem.className = "active"
+class Nav {
+	constructor(navElems) {
+		this.navElems = document.querySelectorAll(navElems);
+		this.displayAcvtiveLink()
+		this.updateUrl()
 	}
-})
+
+	displayAcvtiveLink() {
+		this.navElems.forEach(elem => {
+			if (elem.href === location.href) {
+				elem.className = "active";
+				elem.href = "#";
+			}
+		})
+	}
+
+	removeUrlHash() {
+		let customUrl = location.href.slice(0, -1);
+		window.history.pushState(location.href, document.title, customUrl);
+	}
+
+	updateUrl() {
+		this.navElems.forEach(elem => {
+			elem.addEventListener('click', () => {
+				if (location.href.includes("#")) {
+					console.log("yes")
+				}
+			})
+		})
+	}
+}
+
+
+let nav = new Nav(".nav-links a")
